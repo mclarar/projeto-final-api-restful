96fcabd4c2ad871b7e1ee94d2cdf3be7e9351d42
Amanda Corrêa <101524234+amandaccorrea@users.noreply.github.com>
2022-05-31 21:32:03 -0300

    adicionando entidades

---
+++ src/main/java/com/residencia/ecommerce/entity/Categoria.java
@@ -0,0 +1,59 @@
+package com.residencia.ecommerce.entity;
+
+
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_categoria")
+	private Integer idCategoria;
+	
+	@Column(name = "nome")
+	private String nomeCategoria;
+	
+	@Column(name = "descricao")
+	private String descricaoCategoria;
+	
+	@OneToMany(mappedBy = "categoria")
+	private List<Produto> produtoList;
+
+	public Integer getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(Integer idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	public String getNomeCategoria() {
+		return nomeCategoria;
+	}
+
+	public void setNomeCategoria(String nomeCategoria) {
+		this.nomeCategoria = nomeCategoria;
+	}
+
+	public String getDescricaoCategoria() {
+		return descricaoCategoria;
+	}
+
+	public void setDescricaoCategoria(String descricaoCategoria) {
+		this.descricaoCategoria = descricaoCategoria;
+	}
+	
+	
+	
+	
+}
---
+++ src/main/java/com/residencia/ecommerce/entity/Cliente.java
@@ -0,0 +1,118 @@
+package com.residencia.ecommerce.entity;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_cliente")
+	private Integer idCliente;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "nome_completo")
+	private String nomeCliente;
+	
+	@Column(name = "cpf")
+	private String cpf;
+	
+	@Column(name = "telefone")
+	private String telefoneCliente;
+	
+	@Column(name = "data_nascimento")
+	private Date dataNascimento;
+	
+	@OneToMany
+	@JoinColumn(name = "id_endereco", referencedColumnName = "id_endereco")
+	private Endereco idEndereco;
+	
+	@OneToMany
+	@JoinColumn(name = "id_pedido", referencedColumnName = "id_pedido")
+	private Pedido idPedido;
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidoList;
+
+	public Integer getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Integer idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefoneCliente() {
+		return telefoneCliente;
+	}
+
+	public void setTelefoneCliente(String telefoneCliente) {
+		this.telefoneCliente = telefoneCliente;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Endereco getIdEndereco() {
+		return idEndereco;
+	}
+
+	public void setIdEndereco(Endereco idEndereco) {
+		this.idEndereco = idEndereco;
+	}
+
+	public Pedido getIdPedido() {
+		return idPedido;
+	}
+
+	public void setIdPedido(Pedido idPedido) {
+		this.idPedido = idPedido;
+	}
+
+	
+	
+	
+	
+	
+}
